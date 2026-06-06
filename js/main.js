e8d28c56d8c1d8a0e24ece1cf4227cb0c1b9d618
TheStxfan <stefan.ferro5@gmail.com>
2026-06-06 22:20:05 +0200

    feat: added poster images to movies and series injection

--- js/main.js
+++ js/main.js
@@ -1,28 +1,47 @@
 // Importing api functions
 import { getTrendingMovies, getTrendingTvShows } from "./api.js";
 
-// Injecting Trending Movies
+// Results
 const trendingMovies = (await getTrendingMovies()).results
-const movieIniezione = document.getElementById("movieIniezione")    // crea una costante che punta all'elemento con id="movieIniezione" (0)
+const trendingSeries = (await getTrendingTvShows()).results
+const posterBaseUrl = "https://image.tmdb.org/t/p/w220_and_h330_face"
+
+// Testing
+
+// Injecting Trending Movies
+const movieIniezione = document.getElementById("movieIniezione")
 
 trendingMovies.map((item) => {
-    const movieBox = document.createElement("div")  // crea un nuovo div (1)
-    movieBox.classList = "movie"                    // assegna al div (1) la classe movie 
-    const movieH2 = document.createElement("h2")    // crea una costante contenente l'elemento H2 (2)
-    movieH2.innerText = item.title                  // assegna come testo a quella costante H2 (2) il titolo del film (item)
-    movieBox.appendChild(movieH2)                   // inserisci la costante H2 (2) nel div (1)
-    movieIniezione.append(movieBox)                 // inserisci il div (1) in movieIniezione (0)
+    const movieBox = document.createElement("div")
+    const movieH2 = document.createElement("h2")
+    const moviePoster = document.createElement("img")
+
+    movieBox.classList = "movie"
+
+    movieH2.innerText = item.title
+    moviePoster.src = `${posterBaseUrl}${item.poster_path}`
+    moviePoster.alt = item.title
+
+    movieBox.appendChild(moviePoster)
+    movieBox.appendChild(movieH2)
+    movieIniezione.append(movieBox)
 })
 
 // Injecting Trending Series
-const trendingSeries = (await getTrendingTvShows()).results
-const serieIniezione = document.getElementById("serieIniezione")    // crea una costante che punta all'elemento con id="movieIniezione" (0)
+const serieIniezione = document.getElementById("serieIniezione")
 
 trendingSeries.map((item) => {
-    const serieBox = document.createElement("div")  // crea un nuovo div (1)
-    serieBox.classList = "movie"                    // assegna al div (1) la classe movie 
-    const serieH2 = document.createElement("h2")    // crea una costante contenente l'elemento H2 (2)
-    serieH2.innerText = item.name                  // assegna come testo a quella costante H2 (2) il titolo del film (item)
-    serieBox.appendChild(serieH2)                   // inserisci la costante H2 (2) nel div (1)
-    serieIniezione.append(serieBox)                 // inserisci il div (1) in movieIniezione (0)
+    const serieBox = document.createElement("div")
+    const serieH2 = document.createElement("h2")
+    const seriePoster = document.createElement("img")
+
+    serieBox.classList = "movie"
+
+    serieH2.innerText = item.name
+    seriePoster.src = `${posterBaseUrl}${item.poster_path}`
+    seriePoster.alt = item.name
+
+    serieBox.appendChild(seriePoster)
+    serieBox.appendChild(serieH2)
+    serieIniezione.append(serieBox)
 })
